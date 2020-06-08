7beb7e18d5b1ee4acd041f8a416b19ca416870c7
thiago mendes <thimens@hotmail.com>
2020-06-07 22:16:49 -0700

    game simulator updated

--- tests/MiniPoker.GameSimulator/Program.cs
+++ tests/MiniPoker.GameSimulator/Program.cs
@@ -10,50 +10,66 @@ namespace MiniPoker.GameSimulator
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Welcome to MiniPoker Simulator!");
-            Console.WriteLine();
+            Console.WriteLine("Welcome to MiniPoker Simulator!");            
             Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
 
-            var totalGames = 0;
-            while (totalGames < 1 || totalGames > 30)
+            var repeat = 'Y';
+
+            while (repeat == 'Y')
             {
-                Console.WriteLine("How many games would you like to simulate? (1-30):");
-                var line = Console.ReadLine();
-                if (int.TryParse(line, out int result))
-                    totalGames = result;
-            }
+                var totalGames = 0;
+                while (totalGames < 1 || totalGames > 30)
+                {                    
+                    Console.WriteLine("How many games would you like to simulate? (1-30):");
+                    var line = Console.ReadLine();
+                    if (int.TryParse(line, out int result))
+                        totalGames = result;
+                }
 
-            var game = new Game();
-            for (int round = 0; round < totalGames; round++)
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine(new String('-', 15));
-                Console.WriteLine();
-                Console.WriteLine("Staring new game..");
-                Console.WriteLine("Dealing cards..");
+                var game = new Game();
+                for (int round = 0; round < totalGames; round++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(new String('-', 15));
+                    Console.WriteLine();
+                    Console.WriteLine("Staring new game..");
+                    Console.WriteLine("Dealing cards..");
 
-                var players = game.Deal("Jen", "Mike", "Bob", "Alice");
+                    var players = game.Deal("Jen", "Mike", "Bob", "Alice");
 
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine();
-                Console.WriteLine("Players:");
-                Console.ForegroundColor = ConsoleColor.White;                
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine();
+                    Console.WriteLine("Players:");
+                    Console.ForegroundColor = ConsoleColor.White;
 
-                foreach (var player in players)
-                    Console.WriteLine($"{player.Name} \t {string.Join(" ", player.Cards.Select(c => c.ToString().PadLeft(3)))}");
+                    foreach (var player in players)
+                        Console.WriteLine($"{player.Name} \t {string.Join(" ", player.Cards.Select(c => c.ToString().PadLeft(3)))}");
 
-                var winners = game.GetWinners(players);
+                    var winners = game.GetWinners(players);
 
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine();
-                Console.WriteLine("Winner:");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine(string.Join(" & ", winners.Select(w => w.Name)));
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine();
+                    Console.WriteLine("Winner:");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(string.Join(" & ", winners.Select(w => w.Name)));
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                repeat = 'A';
+                while (repeat != 'Y' && repeat != 'N')
+                {
+                    Console.WriteLine("Would you like to simulate again? (Y-N):");
+                    repeat = Char.ToUpper((char)Console.ReadKey().KeyChar);
+                    Console.WriteLine();
+                }
                 Console.WriteLine();
             }
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Hit any key to exit");
+            Console.WriteLine("Press any key to exit..");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey();
         }
     }
